e8ac9c6cb10a8521eb2d226e6674fa9ad9415147
Pyae Sone Shin Thant <pssthant@gmail.com>
2026-02-25 18:58:38 +0700

    feat(api): add Swagger with CLI plugin and wire global pipes, filters, interceptors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,29 @@
 import 'dotenv/config'
-import { NestFactory } from '@nestjs/core'
+import { NestFactory, Reflector } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
+import { HttpExceptionFilter } from './common/filters/http-exception.filter'
+import { ResponseInterceptor } from './common/interceptors/response.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: false })
+  const reflector = app.get(Reflector)
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
+  app.useGlobalFilters(new HttpExceptionFilter())
+  app.useGlobalInterceptors(new ResponseInterceptor(reflector))
+
+  const config = new DocumentBuilder()
+    .setTitle('Unishare API')
+    .setDescription('API for the Unishare platform')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
+
   await app.listen(process.env.PORT ?? 3001)
 }
 bootstrap()
